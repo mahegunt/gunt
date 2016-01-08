08fcd68e719cb01aca15038b04927d12405a3e24
mahegunt <mahegunt79@gmail.com>
2016-01-08 15:15:41 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+var myApp = angular.module('myApp',[]);
+myApp.controller('AppCtrl',function($scope, $http){
+
+var refresh = function() {
+$http.get('/contactlist').success(function(response){
+    console.log("I got the data I requested");
+    $scope.contactlist = response;
+    $scope.contact= "";
+    });
+};
+
+
+
+refresh();
+
+$scope.addContact = function() {
+console.log($scope.contact);
+$http.post('/contactlist',$scope.contact).success(function(response){
+        console.log(response);
+        refresh();
+    });
+    };
+   
+$scope.remove = function(id) {
+    console.log(id);
+    $http.delete('/contactlist/' + id).success(function(response){
+        refresh();
+    });
+};
+
+});
